dab22b84d55b41958214a4f5cecd07b176191bb8
rogunknow <68379937+rogunknow@users.noreply.github.com>
2020-10-11 21:46:24 +0530

    Update Lanmain.java

--- src/ruchirg/org/midsem/Lanmain.java
+++ src/ruchirg/org/midsem/Lanmain.java
@@ -2,7 +2,7 @@ package ruchirg.org.midsem;
 import ruchirg.org.midsem.Lanclass;
 
 public class Lanmain extends Lanclass{
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args){
 		Lanclass sp=new Lanclass();
 		sp.Lan();
 	}
